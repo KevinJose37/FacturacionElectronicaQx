d08c449b90641dae275c13218557c323b8a352a6
KevinJose37 <kevinjjben@gmail.com>
2026-05-10 18:34:08 -0500

    feat: add uvicorn execution entry point to main module

--- main.py
+++ main.py
@@ -95,3 +95,4 @@ if __name__ == "__main__":
         import selectors
         loop = asyncio.SelectorEventLoop(selectors.SelectSelector())
         asyncio.set_event_loop(loop)
+    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=False)
